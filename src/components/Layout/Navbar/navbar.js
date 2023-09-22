20cc1c291cbdbf0758471b6e0dcd7a45f110df3d
Dhanush <dhanushprabhu0604@gmail.com>
2023-09-22 13:18:48 +0530

    nav linked

--- src/components/Layout/Navbar/navbar.js
+++ src/components/Layout/Navbar/navbar.js
@@ -4,18 +4,18 @@ import "./navstyle.css"
 const navbar = () => {
   return (
     <header className="nav_bar">
-      <h1 className="logo">Samraksha sena</h1>
+      <h1 className="logo"> <a href='/'>  Samraksha sena </a>  </h1>
       <nav>
         <ul class="nav_link">
-          <li> <a href=""> Home </a></li>
-          <li> <a href=""> Map </a> </li>
+          <li> <a href="/"> Home </a></li>
+          <li> <a href="/map"> Map </a> </li>
           <li> <a href=""> Agencies</a></li>
           <li> <a href=""> Contact</a></li>
-          <li> <a href=""> Chat Room</a></li>
+          <li> <a href="/communication"> Chat Room</a></li>
         </ul>
       </nav>
       <ul className="nav_login">
-        <li> <a href=""> Login <b>|</b> </a> </li>
+        <li> <a href="/login"> Login <b>|</b> </a> </li>
         <li> <a href=""> English </a> </li>
       </ul>
     </header>
